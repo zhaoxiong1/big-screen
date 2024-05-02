e52058f52556be0b47ec745365e26b3c4d09bbc8
mingzhuchuhai <1620532963@qq.com>
2024-05-02 23:48:06 +0800

    aa

--- src/views/footer/index.vue
+++ src/views/footer/index.vue
@@ -5,11 +5,15 @@
     <!--头部开始-->
     <div class="button-group">
       <div class="button-row">
-        <button class="button" @click="handleClick('Button 1')">简介</button>
-        <button class="button" @click="handleClick('Button 2')">古诗</button>
-        <button class="button" @click="handleClick('Button 3')">习俗</button>
-        <button class="button" @click="handleClick('Button 4')">画作</button>
-        <button class="button " @click="handleClick('Button 5')">书籍</button>
+        <button class="button intro-button" @click="handleClick('Button 1')">简介<span class="button-icon"></span>  </button>
+        <button class="button poetry-button" @click="handleClick('Button 2')">古诗<span class="button-icon"></span>  </button>
+        <button class="button custom-button" @click="handleClick('Button 3')">习俗<span class="button-icon"></span>  </button>
+        <button class="button pic-button" @click="handleClick('Button 4')">画作<span class="button-icon"></span>  </button>
+        <button class="button book-button" @click="handleClick('Button 5')">
+          
+          书籍
+          <span class="button-icon"></span>  
+        </button>
       </div>
       <div class="button-row">
         <div class="home-div">
@@ -18,8 +22,8 @@
           <div class="home-div1">
           </div>
           <div class="home-div2">
-            <button class="button home-button " @click="gotoHome('Button 5')">
-              <span>☖</span>返回主页</button>
+            <button class="button home-button button-with-icon" @click="gotoHome('Button 5')">
+              返回主页</button>
           </div>
         </div>
 
@@ -61,6 +65,132 @@
 
 
 <style>
+
+.book-button {  
+  /* 确保按钮有足够的 padding-top 来容纳图标 */  
+  padding-top: 40px; /* 根据你的图标大小调整 */  
+  position: relative; /* 用于定位图标 */  
+  /* 其他样式保持不变 */  
+}  
+.pic-button{
+  padding-top: 40px; /* 根据你的图标大小调整 */  
+  position: relative; /* 用于定位图标 */ 
+}
+.custom-button{
+  padding-top: 40px; /* 根据你的图标大小调整 */  
+  position: relative; /* 用于定位图标 */ 
+}
+.poetry-button{
+  padding-top: 40px; /* 根据你的图标大小调整 */  
+  position: relative; /* 用于定位图标 */ 
+}
+.intro-button{
+  padding-top: 40px; /* 根据你的图标大小调整 */  
+  position: relative; /* 用于定位图标 */ 
+}
+  
+.book-button .button-icon {  
+  display: block;  
+  position: absolute;  
+  top: -49px; /* 根据需要调整图标与按钮顶部的距离 */  
+  left: 50%;  
+  transform: translateX(-50%); /* 水平居中图标 */  
+  width: 60px; /* 图标宽度 */  
+  height: 60px; /* 图标高度 */  
+  border-radius: 50%; /* 圆形图标 */  
+  background-color: #7de0f9; /* 示例颜色，当图片加载失败时显示 */  
+  background-image: url('../../assets/button/book1.jpg');  
+  background-size: contain; /* 保持图片的宽高比 */  
+  background-position: center; /* 确保图片在元素内居中 */  
+  background-repeat: no-repeat; /* 不重复图片 */  
+    
+} 
+.pic-button .button-icon {  
+  display: block;  
+  position: absolute;  
+  top: -49px; /* 根据需要调整图标与按钮顶部的距离 */  
+  left: 50%;  
+  transform: translateX(-50%); /* 水平居中图标 */  
+  width: 60px; /* 图标宽度 */  
+  height: 60px; /* 图标高度 */  
+  border-radius: 50%; /* 圆形图标 */  
+  background-color: #7de0f9; /* 示例颜色，你可以使用背景图片 */  
+  background-image: url('../../assets/button/book3.webp');  
+  background-size: cover;  
+} 
+.custom-button .button-icon {  
+  display: block;  
+  position: absolute;  
+  top: -49px; /* 根据需要调整图标与按钮顶部的距离 */  
+  left: 50%;  
+  transform: translateX(-50%); /* 水平居中图标 */  
+  width: 60px; /* 图标宽度 */  
+  height: 60px; /* 图标高度 */  
+  border-radius: 50%; /* 圆形图标 */  
+  background-color: #7de0f9; /* 示例颜色，你可以使用背景图片 */  
+  /* background-image: url('../../assets/button/book.jpg');   */
+  background-size: cover;  
+} 
+.poetry-button .button-icon {  
+  display: block;  
+  position: absolute;  
+  top: -49px; /* 根据需要调整图标与按钮顶部的距离 */  
+  left: 50%;  
+  transform: translateX(-50%); /* 水平居中图标 */  
+  width: 60px; /* 图标宽度 */  
+  height: 60px; /* 图标高度 */  
+  border-radius: 50%; /* 圆形图标 */  
+  background-color: #7de0f9; /* 示例颜色，你可以使用背景图片 */  
+  /* background-image: url('../../assets/button/book.jpg');   */
+  background-size: cover;  
+} 
+.intro-button .button-icon {  
+  display: block;  
+  position: absolute;  
+  top: -49px; /* 根据需要调整图标与按钮顶部的距离 */  
+  left: 50%;  
+  transform: translateX(-50%); /* 水平居中图标 */  
+  width: 60px; /* 图标宽度 */  
+  height: 60px; /* 图标高度 */  
+  border-radius: 50%; /* 圆形图标 */  
+  background-color: #7de0f9; /* 示例颜色，你可以使用背景图片 */  
+  /* background-image: url('../../assets/button/book.jpg');   */
+  background-size: cover;  
+} 
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+.button-with-icon {  
+    display: inline-flex; /* 使图片和文字并排显示 */  
+    align-items: center; /* 垂直居中 */  
+    padding: 10px 20px 10px 40px; /* 根据需要调整内边距，左侧留出图标的空间 */  
+    background-image: url('../../assets/button/home.png'); /* 设置图标路径 */  
+    background-position: left center; /* 设置图标位置 */  
+    background-repeat: no-repeat; /* 不重复背景图片 */  
+    background-size: 33px 33px; /* 设置图标大小 */  
+    border: none; /* 移除边框 */  
+    border-radius: 5px; /* 添加圆角 */  
+    cursor: pointer; /* 鼠标悬停时变为小手形状 */  
+    background-position: left center;
+    background-position-x: 27px
+  } 
+ 
+
+
 .button-group {  
   display: flex;  
   flex-direction: column; /* 垂直排列按钮组 */  
@@ -112,7 +242,7 @@
 .home-button {  
   /* 如果有特殊的home按钮样式，可以在这里添加 */  
  /* 如果有特殊的home按钮样式，可以在这里添加 */ 
- text-align: center; 
+ text-align: right; 
  background-color: #7cc4d6;
   color: #4699ae;  
   margin: 0; 
@@ -125,22 +255,15 @@
     -1px 1px 0 #fff,
      1px 1px 0 #fff; /* 阴影模拟边框 */
   padding: 10px; /* 文字周围的填充，可以增加白边的大小 */
-  line-height: 38px;
   
 }  
   
-.home-button span{
-  color:white;
-  font-size: 22px;
-  line-height: 38px;
-
-}
 .home-button:hover {  
   background-color: #55838e; /* 举例：hover状态下的颜色 */  
 }  
-.button:hover {  
-  background-color: #63bcd3; /* 举例：hover状态下的颜色 */  
-}  
+/* .button:hover {  
+  background-color: #63bcd3;
+}   */
 
 
 .home-button .fas { /* 可能需要调整选择器来正确应用样式 */  
